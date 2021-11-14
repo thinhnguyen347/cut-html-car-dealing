fbe6064e5b2469b3f0cbc7d83a9668d080cbfacf
thinhnguyen347 <thinh.ohg@gmail.com>
2021-11-15 01:18:31 +0700

    update

---
+++ .history/js/main_20211115010428.js
@@ -0,0 +1,26 @@
+let mobileItem = $(".menu-mobile-item");
+
+mobileItem.each((index) => {
+  mobileItem.eq(index).on("click", () => {
+    mobileItem.removeClass("active");
+    mobileItem.eq(index).addClass("active");
+  });
+});
+
+mobileItem.eq(2).on("click", () => {
+  $('input.model').val("")
+});
+
+mobileItem.click(() => {
+    $(".pop-up").removeClass("show");
+  });
+
+mobileItem.last().click(() => {
+  $(".pop-up").toggleClass("show");
+});
+
+$('.pop-up > p').click(() => {
+    $(".pop-up").removeClass("show");
+  });
+
+
