d9993a4fc725e2e49bda01e9d410631ba3806a11
thinhnguyen347 <thinh.ohg@gmail.com>
2021-11-14 15:35:13 +0700

    update

---
+++ .history/main_20211114152554.js
@@ -0,0 +1,7 @@
+$(document).ready(function () {
+    $(".menu-m").addClass("hide")
+    $(".menu-icon").click(function () {
+        $(".menu-mobile").toggleClass("hide");
+    })
+
+})
---
+++ .history/main_20211114152605.js
@@ -0,0 +1,6 @@
+
+    $(".menu-icon").click(function () {
+        $(".menu-mobile").toggleClass("hide");
+    })
+
+})
